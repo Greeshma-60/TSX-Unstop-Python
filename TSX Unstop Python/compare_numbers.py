187c7410a6dd29eee9633ca803ddd0205c9b594c
Greeshma-60 <tadiboyinagreeshma@gmail.com>
2025-05-17 19:46:06 +0530

    Add files via upload

---
+++ TSX Unstop Python/compare_numbers.py
@@ -0,0 +1,9 @@
+# compare two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("Firset number is greater")
+elif a < b:
+    print("First number is smaller")
+else:
+    print("both numbers are equal")
